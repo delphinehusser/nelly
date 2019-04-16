b42c392f33d7e135fc8cbbc4a6f279fb61c80cd3
Taratata1 <49532469+Taratata1@users.noreply.github.com>
2019-04-16 14:32:34 +0200

    Add files via upload

--- coord.cpp
+++ coord.cpp
@@ -1,45 +1,30 @@
 #include <cstdlib>
-#include "code.hpp"
-#include "coord.cpp"
-#include "animal.cpp"
-#include "ens.cpp"
-#include "grille.cpp"
 #include <iostream>
-#include <chrono>;
+#include <chrono>
 
 using namespace std;
 
 /** Infrastructure minimale de test **/
 #define ASSERT(test) if (!(test)) cout << "Test failed in file " << __FILE__ << " line " << __LINE__ << ": " #test << endl
 
-
 // CREER COORD //
+
+struct Coord{
+  int x;
+  int y;
+};
 
 Coord creerCoord(int abs, int ord){
   Coord point;
   point.x = abs;
   point.y = ord;
+  return point;
 }
 
 void afficheCoord (Coord c){
   cout << "(" << c.x << "," << c.y << ")" << endl;
 }
 
-// EGAL COORD //
-
-bool egalCoord(Coord c1, Coord c2){
-  if (c1.x == c2.x){
-    if (c1.y == c2.y){
-      return true;
-    }
-  }
-  return false;
-}
-
-void testEgalCoord(){
-  ASSERT( egalCoord(creerCoord(4,5),creerCoord(4,5)) );
-  ASSERT( egalCoord(creerCoord(3,6),creerCoord(3,6)) );
-}
 
 // GET X //
 
@@ -55,11 +40,32 @@ int getY(Coord c){
   return ord;
 }
 
+
 // EGAL COORD //
 
 bool egalCoord(Coord c1, Coord c2){
-  if ((c1.x == c2.x) and (c1.x == c2.y)){
-    return true;
+  if (c1.x == c2.x){
+    if (c1.y == c2.y){
+      return true;
+    }
   }
   return false;
 }
+
+
+void testEgalCoord(){
+  ASSERT (egalCoord(creerCoord(1,2),creerCoord(1,2)) == true);
+}
+
+
+int main(){
+  Coord c1 = creerCoord(2,1);
+  afficheCoord(c1);
+  cout << endl;
+
+  testEgalCoord;
+  return 0;
+}
+
+
+
