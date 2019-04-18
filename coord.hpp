9b4a2b7a04d368e47eb3eb74040df51f62dedd7b
Taratata1 <49532469+Taratata1@users.noreply.github.com>
2019-04-18 10:36:36 +0200

    Update coord.hpp

--- coord.hpp
+++ coord.hpp
@@ -7,4 +7,14 @@ using namespace std;
 
 #include "coord.cpp"
 
+struct Coord{
+  int x;
+  int y;
+};
+
+coord creerCoord(int x, int g);
+int getX(coord c);
+int getY(coord c);
+bool egalCoord(Coord c1, Coord c2);
+
 
