78f706222f81f7c2350cd8af60e05b3d3d988641
Taratata1 <49532469+Taratata1@users.noreply.github.com>
2019-04-18 10:43:27 +0200

    Update enscoord.hpp

--- enscoord.hpp
+++ enscoord.hpp
@@ -6,3 +6,19 @@
 using namespace std;
 
 #include "enscoord.cpp"
+
+struct Ens {
+  Coord point[];
+  int nbPoint;
+};
+
+void ensVide(Ens ec);
+void afficheEc(Ens ec);
+Ens creerEc();
+void ajouteEc(Ens ec,Coord c);
+int cardEc(Ens ec);
+bool estVideEc(Ens ec);
+bool estDansEc(Ens ec, Coord c);
+Coord trouverVoisin(Coord c);
+Ens supprimeEc(Ens &ec, Coord c);
+Coord randomEC(Ens ec);
